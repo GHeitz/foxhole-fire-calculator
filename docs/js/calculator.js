74493332238469c0a9b7b1e26e806abfb6f2327a
Mitsuwa <46683717+GHeitz@users.noreply.github.com>
2026-08-03 16:45:52 +0200

    Add fire calculation engine

--- docs/js/calculator.js
+++ docs/js/calculator.js
@@ -1,1 +1,60 @@
+/**
+ * Foxhole Fire Calculator
+ * Core calculation engine
+ */
 
+
+function calculateFire(
+    weapon,
+    building,
+    shots
+) {
+
+    const fireStack =
+        weapon.fireStack * shots;
+
+
+    const effectiveFire =
+        fireStack *
+        (1 - building.fireResistance);
+
+
+    let intensity;
+
+
+    if (effectiveFire < 5) {
+
+        intensity = "LOW";
+
+    } 
+    else if (effectiveFire < 15) {
+
+        intensity = "MEDIUM";
+
+    } 
+    else if (effectiveFire < 30) {
+
+        intensity = "HIGH";
+
+    } 
+    else {
+
+        intensity = "INFERNO";
+
+    }
+
+
+    return {
+
+        fireStack:
+            fireStack.toFixed(2),
+
+        effectiveFire:
+            effectiveFire.toFixed(2),
+
+        intensity:
+            intensity
+
+    };
+
+}
